1b82df6d09168f4f9a57c54a5b6e2ad0db4fb4b7
Souvik3798 <souvikbarua693@gmail.com>
2022-06-18 18:36:18 +0530

    homepage navbar solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -581,8 +581,8 @@
     <!-- end scroll up button -->
     <!-- Bootstrap JavaScript Libraries -->
 
-    <script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js " integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p " crossorigin="anonymous "></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js " integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF " crossorigin="anonymous "></script>
+    {{-- <script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js " integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p " crossorigin="anonymous "></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js " integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF " crossorigin="anonymous "></script> --}}
 
     <!-- carousel cdn  -->
     <!-- carousel jquery -->
@@ -632,11 +632,11 @@
     })
    </script>
     <!-- typing animation script -->
-    @php
-        foreach($portfolios as $portfolio){
-            $name[] = $portfolio->Name;
-        }
-    @endphp
+     @php
+            foreach($portfolios as $portfolio){
+                $name[] = $portfolio->Name;
+            }
+        @endphp
     <script>
         var speed = 200;
         var speed2 = 150;
